274d5da4d1173e73142b3f670c71fdea4239eda8
PrinscoDev <119410301+prince160@users.noreply.github.com>
2025-04-23 19:55:27 +0200

    Update app.py
    Api virus total

--- frontend/app.py
+++ frontend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import requests
 from dotenv import load_dotenv
 import os
@@ -49,14 +49,49 @@ def request_mistral():
         return render_template('index.html', response="Une erreur est survenue lors de la requête.")
     
 
+# VIRUS TOTAL API
+API_KEY_VT = "f4bf09e31b72bc0572320e6a2a3193f4133b39353424087a7e989acde7c6b47c"
+
+@app.route("/analyse_ip")
+def analyse_ip():
+    ip = request.args.get("ip")
+    if not ip:
+        return jsonify({"error": "IP manquante"}), 400
+
+    url = f"https://www.virustotal.com/api/v3/ip_addresses/{ip}"
+    headers = {
+        "x-apikey": API_KEY_VT
+    }
+
+    response = requests.get(url, headers=headers)
+
+    if response.status_code != 200:
+        return jsonify({"error": "Erreur avec VirusTotal"}), response.status_code
+
+    data = response.json()
+
+    #récupérer juste le nombre de détections "malicious"
+    stats = data.get("data", {}).get("attributes", {}).get("last_analysis_stats", {})
+    malicious_count = stats.get("malicious", 0)
+
+    # Renvoie un niveau simple pour le front
+    criticity = min(malicious_count * 20, 100)  # niveau sur 100, simple échelle
+
+    return jsonify({
+        "ip": ip,
+        "malicious": malicious_count,
+        "criticity": criticity
+    })
+    
+
 # --- MOCK DATA à remplacer par la vraie analyse plus tard (les retours à envoyer au front)---
 mock_data = {
     "user_name": "prince",
-    "ip_suspect": 42,
+    "ip_suspect": "192.168.1.156",
     "machine": "hp_buro",
     "protocols": "TCP, UDP, HTTP, DNS",
     "packet_count": 1087,
-    "message": "Activité réseau normale détectée. Aucun comportement malveillant repéré dans ce segment.",
+    "message": "Cet IP a été repérée en train de faire des trucs louches : elle envoie plein de requêtes bizarres à des sites suspects, un peu comme quelqu’un qui toque à toutes les portes d’un quartier à 3h du matin. Notre système l’a croisée plusieurs fois dans des listes noires connues. Bref, c’est clairement pas net. Prudence recommandée !",
     # "alerts": [
     #     {"ip": "192.168.0.101", "description": "Comportement suspect détecté sur le port 4444."},
     #     {"ip": "10.0.0.8", "description": "Connexion anormale à un domaine blacklisté."}
